26bd396de4ca05089e5769b0643daae6a3a6f6c6
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-10-23 23:56:58 -0500

    Week 1 Push

--- week-1/weight-converter/weight-converter.js
+++ week-1/weight-converter/weight-converter.js
@@ -1,10 +1,34 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Nicholas Skelton
+ * Date: 10.23.25
+ * File Name: weight-converter.js
+ * Description: Pounds to Kilograms Weight Converter
 */
 
 "use strict";
 
 // TODO: Implement the weight conversion logic here
+
+function convertWeight() {
+  const pounds = process.argv[2];
+
+  // Error if incorrect amount of command line arguements
+  if (process.argv.length != 3) {
+    console.error("Usage: node weight-converter.js <pounds>");
+    process.exit(1);
+  }
+
+  // Error if input is not a number
+  if (isNaN(pounds)) {
+    console.error("Input must be a number.");
+    process.exit(1);
+  }
+
+  // Convert pounds to kilograms
+  const kilos = (pounds * .454).toFixed(2);
+
+  // Display the conversion of pounds to kilograms
+  console.log(`${pounds} pounds is equal to ${kilos} kilograms.`);
+}
+
+convertWeight();
