ad1248a711077272e34452b863aac1053b4f7f93
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-11-02 01:58:22 -0500

    Week 2 Push

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,19 @@
 /**
- * Author:
- * Date:
- * File Name:
+ * Author: Nicholas Skelton
+ * Date: 11.1.25
+ * File Name: index.js
  * Description:
 */
 
 // TODO: Import your module using require
+const { createRecipe, setTimer, quit } = require("./recipes");
 
 // TODO: Implement your CLI program here
+const newRecipe = createRecipe(["horseradish", "oysters", "chocolate"]);
+console.log(newRecipe);
+
+const newTimer = setTimer(47);
+console.log(newTimer);
+
+const newQuit = quit();
+console.log(newQuit);
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,41 @@
 /**
- * Author:
- * Date:
- * File Name:
+ * Author: Nicholas SKelton
+ * Date: 11.1.25
+ * File Name: recipes.js
  * Description:
 */
 
 // Define the createRecipe function
+
+/* This function took me a while to get right, and I'm still not sure if it's the "correct" way to
+write it, but it gets the job done. I would welcome any feedback on what would have been a better
+or more efficient way to write it */
+
 function createRecipe(ingredients) {
-  // TODO: Implement this function
+  let recipeList = ""      // Declare recipeList to collect ingredients
+
+  for (i = 0; i < ingredients.length; i++) {
+    if (i !== ingredients.length - 1) {   // If statement for every index 'except' the last one, appending to recipeList with a comma and space
+      recipeList += (ingredients[i] + ", ");
+    } else {
+      recipeList += ingredients[i];     // Else statement for first (and only) index that isn't not the last one, appending to recipeList but not adding a comma
+    }
+  }
+  return "Recipe created with ingredients: " + recipeList;   // Console log with recipeList
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
-  // TODO: Implement this function
+  return `Timer set for ${minutes} minutes`;
 }
 
 // Define the quit function
 function quit() {
-  // TODO: Implement this function
+  return "Program exited";
 }
 
-// TODO: Export the functions
+module.exports = {
+  createRecipe: createRecipe,
+  setTimer: setTimer,
+  quit: quit,
+}
