62e4aba88542f8c750cbf30d9f54a36c61cb57f1
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-11-09 12:45:49 -0600

    Week 3 Push

--- week-3/distance-calculator/src/distance-calculator.js
+++ week-3/distance-calculator/src/distance-calculator.js
@@ -1,5 +1,26 @@
 function calculateDistance(planet1, planet2) {
-  // TODO: Implement this function
+
+  // AU from Sun list
+  const planetDist = {
+    mercury: 0.39,
+    venus: 0.72,
+    earth: 1,
+    mars: 1.52,
+    jupiter: 5.2,
+    saturn: 9.5,
+    uranus: 19.2,
+    neptune: 30.1,
+    pluto: 39.5,
+  };
+
+  // Convert arguments to lowercase to match AU list
+  planet1 = planet1.toLowerCase();
+  planet2 = planet2.toLowerCase();
+
+  // Subtract planet1 and planet2, rounding to 2 digits after decimal, and converting it to positive via absolute value
+  const totalDistance = Math.abs(planetDist[planet1] - planetDist[planet2]).toFixed(2);
+
+  return Number(totalDistance);
 }
 
 module.exports = calculateDistance;
--- week-3/distance-calculator/test/distance-calculator.spec.js
+++ week-3/distance-calculator/test/distance-calculator.spec.js
@@ -1,8 +1,44 @@
 const assert = require('assert');
 const calculateDistance = require('../src/distance-calculator');
 
-function testFunctionDescription() {
-  // TODO: Implement this function
+// Test 1 - Simple distance
+function testEarthToMars() {
+  try {
+    assert.strictEqual(calculateDistance("Earth", "Mars"), 0.52);
+    console.log("Test passed!");
+    return true;
+  } catch (error) {
+    console.error(`Failed testEarthToMars: ${error.message}`);
+    return false;
+  }
+}
+
+// Test 2 - Case sensitivity
+function testMercuryToPluto() {
+  try {
+    assert.strictEqual(calculateDistance("mERcURy", "pLutO"), 39.11);
+    console.log("Test passed!");
+    return true;
+  } catch (error) {
+    console.error(`Failed testMercuryToPluto: ${error.message}`);
+    return false;
+  }
+}
+
+// Test 3 - Planet order
+function testSaturnToVenus() {
+  try {
+    assert.strictEqual(calculateDistance("Saturn", "Venus"), 8.78);
+    console.log("Test passed!");
+    return true;
+  } catch (error) {
+    console.error(`Failed testSaturnToVenus: ${error.message}`);
+    return false;
+  }
 }
 
 // Call your test functions here
+
+testEarthToMars();
+testMercuryToPluto();
+testSaturnToVenus();
