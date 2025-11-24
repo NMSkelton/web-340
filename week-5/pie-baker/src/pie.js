3b1a9e3e9819ee3f41999ecd9c5b9a1d32973d39
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-11-23 18:21:13 -0600

    Week 5 Push

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,22 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Nicholas Skelton
+ * Date: 11.23.25
+ * File Name: pie.js
+ * Description: Pie
  */
 "use strict";
 
 function bakePie(pieType, ingredients) {
-  // Your code here
+  const essIngredients = ["flour", "sugar", "butter"];
+
+  for (let item of essIngredients) {
+    if (ingredients.includes(item) === false) {
+      console.warn(`Missing essential ingredient: ${item}`);
+      process.exit(1);
+    }
+  }
+
+  return `Successfully baked a ${pieType} pie!`;
 }
 
 module.exports = { bakePie };
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,12 +1,52 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Nicholas SKelton
+ * Date: 11.23.25
+ * File Name: pie.spec.js
+ * Description: Test
  */
 
 "use strict";
 
 const { bakePie } = require("../src/pie");
 
-// Your tests here
+const exit = jest.spyOn(process, "exit").mockImplementation(code => code);
+
+describe("bakePie", () => {
+  let warn;
+
+  beforeEach(() => {
+    warn = jest.spyOn(console, "warn");
+  });
+
+  afterEach(() => {
+    warn.mockRestore();
+  });
+
+  //Successful test
+
+  test("Successful test if all essIngredients are present", () => {
+    const result = bakePie("sweet potato", ["flour", "sugar", "butter"]);
+
+    expect(result).toBe("Successfully baked a sweet potato pie!");
+    expect(warn).not.toHaveBeenCalled();
+    expect(exit).not.toHaveBeenCalled();
+  });
+
+  // Test for missing ingredient: flour
+
+  test("Exits if flour is missing.", () => {
+    bakePie("apple", ["sugar", "butter"]);
+
+    expect(warn).toHaveBeenCalledWith("Missing essential ingredient: flour");
+    expect(exit).toHaveBeenCalledWith(1);
+  });
+
+  // Test for missing ingredient: butter
+
+  test("Exits if butter is missing.", () => {
+    bakePie("pumpkin", ["flour", "sugar"]);
+
+    expect(warn).toHaveBeenCalledWith("Missing essential ingredient: butter");
+    expect(exit).toHaveBeenCalledWith(1);
+  });
+});
