e1a1ab0f83e25583ac061001af01eb47af8b7483
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-12-02 03:26:04 -0600

    Week 6 Push

--- week-6/fantasy-game-character-creation/src/server.js
+++ week-6/fantasy-game-character-creation/src/server.js
@@ -1,10 +1,40 @@
+"use strict";
+
 const http = require('http');
 const url = require('url');
 
-// TODO: Implement your server here
+let createdChar = null;
 
 const server = http.createServer((req, res) => {
-  // TODO: Implement your routes here
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  if (pathname === "/create-character" && req.method === "POST") {
+    const charClass = query.class;
+    const charGender = query.gender;
+    const charFact = query.fact;
+
+    createdChar = {
+      class: charClass,
+      gender: charGender,
+      fact: charFact,
+    }
+
+    res.writeHead(201);
+    res.end(`${charGender} ${charClass} created! Did you know that they are ${charFact}`)
+
+  } else if (pathname === "/confirm-character" && req.method === "POST") {
+
+    res.writeHead(200);
+    res.end(`Your character has been created!`)
+
+  } else if (pathname === "/view-character" && req.method === "GET") {
+
+    res.writeHead(200);
+    res.end(JSON.stringify(createdChar));
+  }
+
 });
 
 server.listen(3000, () => {
--- week-6/fantasy-game-character-creation/test/server.spec.js
+++ week-6/fantasy-game-character-creation/test/server.spec.js
@@ -1,4 +1,89 @@
 const http = require('http');
 const server = require('../src/server');
 
-// TODO: Implement your tests here
+describe(`Character`, () => {
+
+  afterAll(() => {
+    server.close();
+  });
+
+  test(`creates a character`, done => {
+    const options = {
+      hostname: `localhost`,
+      port: 3000,
+      path: '/create-character?class=Mage&gender=Male&fact=a%20member%20of%20the%20Kirin%20Tor?',
+      method: `POST`
+    };
+
+    const req = http.request(options, res => {
+      let data = "";
+
+      res.on('data', chunk => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        expect(res.statusCode).toBe(201);
+        expect(data).toBe(`Male Mage created! Did you know that they are a member of the Kirin Tor?`);
+        done();
+      });
+    });
+
+    req.end();
+  })
+
+    test(`confirms the character`, done => {
+    const options = {
+      hostname: `localhost`,
+      port: 3000,
+      path: '/confirm-character',
+      method: `POST`
+    };
+
+    const req = http.request(options, res => {
+      let data = "";
+
+      res.on('data', chunk => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        expect(res.statusCode).toBe(200);
+        expect(data).toBe(`Your character has been created!`);
+        done();
+      });
+    });
+
+    req.end();
+  })
+
+    test(`views the character`, done => {
+    const options = {
+      hostname: `localhost`,
+      port: 3000,
+      path: '/view-character',
+      method: `GET`
+    };
+
+    const req = http.request(options, res => {
+      let data = "";
+
+      res.on('data', chunk => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        expect(res.statusCode).toBe(200);
+
+        const char = JSON.parse(data);
+
+        expect(char.class).toBe(`Mage`);
+        expect(char.gender).toBe(`Male`);
+        expect(char.fact).toBe(`a member of the Kirin Tor?`)
+        done();
+      });
+    });
+
+    req.end();
+  })
+});
