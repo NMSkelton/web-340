83075047cec89aced04f4a9c620dfca03debee1c
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-12-07 22:33:26 -0600

    Week 7 Push

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -1,17 +1,34 @@
+"use strict";
+
 const { Duplex } = require('stream');
 
 class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
     // TODO: Initialize your class here
+    this.queue = [];
   }
 
   _write(chunk, encoding, callback) {
     // TODO: Implement your _write method here
+    const data = JSON.parse(chunk.toString());
+
+    if (!data.class || !data.gender || !data.fact) {
+      callback(new Error("Invalid data"));
+      return;
+    }
+
+    this.queue.push(`Your character is a ${data.gender} ${data.class}. Did you know they ${data.fact}`);
+    callback();
   }
 
   _read(size) {
     // TODO: Implement your _read method here
+    if (this.queue.length) {
+      this.push(this.queue.shift() + "\n");
+    } else {
+      this.push(null);
+    }
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const CharacterCreator = require('../src/character-creator');
 
 describe('CharacterCreator', () => {
@@ -8,14 +10,46 @@ describe('CharacterCreator', () => {
   });
 
   test("should process data correctly when written to", (done) => {
-    // TODO: Write your test here
+    const character = {
+      class: "Mage",
+      gender: "Male",
+      fact: "are a member of the Kirin Tor."
+    };
+
+    characterCreator.write(JSON.stringify(character));
+    characterCreator.end();
+
+    characterCreator.on("data", (data) => {
+      expect(data.toString().trim()).toBe(
+        `Your character is a Male Mage. Did you know they are a member of the Kirin Tor.`
+      );
+
+      done();
+    });
   });
 
   test("should emit 'error' when invalid data is written", (done) => {
-    // TODO: Write your test here
+    characterCreator.on("error", (err) => {
+      expect(err.message).toBe("Invalid data");
+      done();
+    });
+
+    characterCreator.write(JSON.stringify({ class: "", gender: "Male" }));
   });
 
   test("should transform data correctly when written to", (done) => {
-    // TODO: Write your test here
+    const character = {
+      class: "Mage",
+      gender: "Male",
+      fact: "are a member of the Kirin Tor."
+    };
+    const expectedOutput = `Your character is a Male Mage. Did you know they are a member of the Kirin Tor.`;
+
+    characterCreator.write(JSON.stringify(character), "utf8", () => {
+      characterCreator.on("data", (data) => {
+        expect(data.toString().trim()).toBe(expectedOutput);
+        done();
+      });
+    });
   });
 });
